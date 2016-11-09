5c6f9c0918897ec8f866befc80662e200231fa6b
Eşref Viduşlu <esref.viduslu@gmail.com>
2016-11-09 13:06:04 +0200

    Revert "Feature/seeds"

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,9 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Category.create(title: "Genel", desc: "Genel katagorimiz burasıdır.", color: "red")
 Category.create(title: "RoR", desc: "Ruby on Rails karegorisi burasıdır.", color: "green")
-Category.create(title: "Ruby", desc: "Ruby karegorisi burasıdır.", color: "green")
-Category.create(title: "Rails", desc: "Rails karegorisi burasıdır.", color: "red")
-Category.create(title: "Rails2", desc: "Rails karegorisi burasıdır.", color: "blue")
-Category.create(title: "Rails3", desc: "Rails karegorisi burasıdır.", color: "green")
-Category.create(title: "Rails4", desc: "Rails karegorisi burasıdır.", color: "blue")
 
 Idea.create(title: "Züper Fikir", description: "Süper züper fikir")
 Idea.create(title: "Süper Fikir", description: "Bence bilişim terimleri tekrar düzenlensin")
 Idea.create(title: "Deneme", description: "Test description.")
-Idea.create(title: "Deneme2", description: "Test2 description.")
-Idea.create(title: "Deneme3", description: "Test3 description.")
-Idea.create(title: "Deneme4", description: "Test4 description.")
-Idea.create(title: "Deneme5", description: "Test5 description.")
-Idea.create(title: "Deneme6", description: "Test6 description.")
 
 puts "Seed data create"
