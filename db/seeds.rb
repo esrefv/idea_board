b2c2103db1e2c673afa9c4ebb98af4b0a0fea254
esrefv <esref.viduslu@gmail.com>
2016-11-10 10:25:10 +0300

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Category.create(title: "Genel", desc: "Genel katagorimiz burasıdır.", color: "red")
-Category.create(title: "RoR", desc: "Ruby on Rails karegorisi burasıdır.", color: "green")
-Category.create(title: "Ruby", desc: "Ruby karegorisi burasıdır.", color: "green")
-Category.create(title: "Rails", desc: "Rails karegorisi burasıdır.", color: "red")
-Category.create(title: "Rails2", desc: "Rails karegorisi burasıdır.", color: "blue")
-Category.create(title: "Rails3", desc: "Rails karegorisi burasıdır.", color: "green")
-Category.create(title: "Rails4", desc: "Rails karegorisi burasıdır.", color: "blue")
 
-Idea.create(title: "Züper Fikir", description: "Süper züper fikir")
-Idea.create(title: "Süper Fikir", description: "Bence bilişim terimleri tekrar düzenlensin")
-Idea.create(title: "Deneme", description: "Test description.")
-Idea.create(title: "Deneme2", description: "Test2 description.")
-Idea.create(title: "Deneme3", description: "Test3 description.")
-Idea.create(title: "Deneme4", description: "Test4 description.")
-Idea.create(title: "Deneme5", description: "Test5 description.")
-Idea.create(title: "Deneme6", description: "Test6 description.")
+c1 = Category.create(title: "Genel", desc: "Genel katagorimiz burasıdır.", color: "red")
+c2 = Category.create(title: "RoR", desc: "Ruby on Rails karegorisi burasıdır.", color: "green")
+c3 = Category.create(title: "Ruby", desc: "Ruby karegorisi burasıdır.", color: "green")
+c4 = Category.create(title: "Rails", desc: "Rails karegorisi burasıdır.", color: "red")
+c5 = Category.create(title: "Rails2", desc: "Rails karegorisi burasıdır.", color: "blue")
+c6 = Category.create(title: "Rails3", desc: "Rails karegorisi burasıdır.", color: "green")
+c7 = Category.create(title: "Rails4", desc: "Rails karegorisi burasıdır.", color: "blue")
+
+Idea.create(title: "Züper Fikir", description: "Süper züper fikir", category_id: c1.id)
+Idea.create(title: "Süper Fikir", description: "Bence bilişim terimleri tekrar düzenlensin", category:c2)
+Idea.create(title: "Züper Fikir2", description: "Süper züper fikir", category_id: c1.id)
+Idea.create(title: "Süper Fikir2", description: "Bence bilişim terimleri tekrar düzenlensin", category: c2)
+
+Idea.create(title: "Deneme", description: "Test description.", category:c5)
+Idea.create(title: "Deneme2", description: "Test2 description.", category:c7)
+Idea.create(title: "Deneme3", description: "Test3 description.", category:c4)
+Idea.create(title: "Deneme4", description: "Test4 description.", category:c3)
+Idea.create(title: "Deneme5", description: "Test5 description.", category:c6)
 
 
-Idea.create(title: "Test1", description: "description1.")
-Idea.create(title: "Test2", description: "description2.")
-Idea.create(title: "Test3", description: "description3.")
-Idea.create(title: "Test4", description: "description4.")
+
 puts "Seed data create"
